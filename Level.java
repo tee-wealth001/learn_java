f460c69f078e4ba730e7b45855c04372147a48f8
stephen <stephenola21@gmail.com>
2024-06-21 16:16:01 +0100

    added enum description

--- Level.java
+++ Level.java
@@ -1,3 +1,9 @@
+// enums can have attributes and methods
+// enum contants are public static and final (unchangeable - cannot be overridden)
+//cannot be used to create objects
+// cannot extend other clases
+// it can implement interfaces
+// use for days, months,colors,etc
 public enum Level {
 
     LOW,
