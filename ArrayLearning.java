a0a429aa814af1f795d1fef18fbbb5484f1da460
stephen <stephenola21@gmail.com>
2024-06-24 10:36:57 +0100

    Arraylist, Linkedlist, Hashmap & Hashset

---
+++ ArrayLearning.java
@@ -0,0 +1,138 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class ArrayLearning {
+
+    // constructor is called automatically when the object instance is created
+    public ArrayLearning() {
+
+        System.out.println("Arrays");
+
+        ArrayListLearning();
+
+        LinkedListLearning();
+
+        HashMapLearning();
+
+        HashSetLearning();
+    }
+
+    public static void ArrayListLearning() {
+        // resizeable array and can be modified as much as you can
+        // element of an arraylist are actualy objects
+        // we can use <String>, <Integer>, <Boolean>
+        // it destroys the whole to create a new
+        // mainly used to store and access data
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("volvo");
+        cars.add("benz");
+        cars.add("Ford");
+        cars.add("Nissan");
+        cars.add(0, "Tesla");
+
+        // modifying the array
+        cars.set(1, "BMW");
+
+        // deleting a member of the array
+        cars.remove(4);
+
+        System.out.println("cars >>>>" + cars);
+
+        // getting a member of the array
+        System.out.println("car >>>>>" + cars.get(3));
+
+        // sorting the array
+        // can also sort numbers
+        Collections.sort(cars);
+
+        System.out.println("sorted cars >>>>" + cars);
+
+        // looping through the array by index
+        for (int i = 0; i < cars.size(); i++) {
+
+            String modifiedText = "my " + cars.get(i);
+            cars.set(i, modifiedText);
+        }
+
+        // looping through the array by individual member
+        for (String i : cars) {
+
+            System.out.println("member cars >>>>" + i);
+        }
+
+        System.out.println("cars >>>>" + cars);
+
+        // removing all members
+        cars.clear();
+        System.out.println("cars >>>>" + cars);
+    }
+
+    public static void LinkedListLearning() {
+        // resizeable array and can be modified as much as you can
+        // element of an linkedlist are actualy objects
+        // we can use <String>, <Integer>, <Boolean>
+        // items are placed in containers and linked to others
+        // mainly used to manipulate data
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        linkedList.add(20);
+        linkedList.add(34);
+        linkedList.add(65);
+        linkedList.add(19);
+
+        // reverse the order of sorted list
+        Collections.sort(linkedList, Collections.reverseOrder());
+
+        linkedList.forEach(l -> System.out.println("member >>>>" + l));
+    }
+
+    public static void HashMapLearning() {
+        // stores items in KEY/VALUE pairs
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+
+        hashMap.put("fruits", 20);
+        hashMap.put("Vegetables", 56);
+        hashMap.put("Sweets", 4);
+
+        System.out.println("hashMap >>>>>>" + hashMap);
+
+        // getting a value by the key
+        System.out.println("getting a value by the key >>>>" + hashMap.get("firstName"));
+
+        // remove an element by the key
+        hashMap.remove("Sweets");
+
+        // looping through using the .KeySet()
+        // can also loop using .values()
+        for (String i : hashMap.keySet()) {
+            System.out.println(i + hashMap.get(i));
+        }
+
+        System.out.println("hashMap size >>>>>>" + hashMap.size());
+
+        // remove all
+        hashMap.clear();
+    }
+
+    public static void HashSetLearning() {
+        // every item is uniquie in the collection
+        HashSet<String> hashSet = new HashSet<String>();
+        hashSet.add("banana");
+        hashSet.add("orange");
+        hashSet.add("apple");
+        hashSet.add("coconut");
+        hashSet.add("mango");
+        hashSet.add("banana"); // ignores this since it already exist
+
+        // remove an element
+        hashSet.remove("mango");
+
+        System.out.println(hashSet);
+
+        hashSet.clear();
+    }
+
+}
--- Main.java
+++ Main.java
@@ -3,12 +3,6 @@
 // import package.name.* (all packages)
 //import package.name.Class (single package)
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-
 public class Main {
 
     static void staticMethod() {
@@ -21,67 +15,69 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Second sec = new Second();
-        System.out.println("Hello world!! >>>>>  " + sec.fname + sec.lname + " " + sec.age);
-        sec.doSomething();
+        // Second sec = new Second();
+        // System.out.println("Hello world!! >>>>>  " + sec.fname + sec.lname + " " + sec.age);
+        // sec.doSomething();
 
-        MyCar myCar = new MyCar();
-        myCar.fullThrottle();
-        myCar.speed(200);
-        myCar.fuelConsumption();
+        // MyCar myCar = new MyCar();
+        // myCar.fullThrottle();
+        // myCar.speed(200);
+        // myCar.fuelConsumption();
 
-        Student student = new Student();
-        student.fname = "AliAce";
-        student.lname = "Hamson";
-        student.age = 17;
-        // Try-With-Resources: This construct ensures that the Scanner (or any other
-        // AutoCloseable resource) is closed automatically at the end of the block,
-        // regardless of whether an exception is thrown.
-        try (Scanner scanner = new Scanner(System.in)) {
-            // Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter Discipline");
-            String discipline = scanner.nextLine();
-            student.setDiscipline(discipline);
-            student.eatTime();
-            student.sleepTime();
-            student.workType();
-            // inner class demonstration
-            Student.StudentType studentType = student.new StudentType();
-            String stdTyp = studentType.studentType = "Part Time";
-            int wrkHrs = studentType.displayWorkHours();
-            System.out.println("My name is >>>>>  " + student.fname + student.lname + " and i am " + student.age
-                    + ", i am studying " + student.getDiscipline() + " " + stdTyp + " I work" + wrkHrs + " per week");
+        // Student student = new Student();
+        // student.fname = "AliAce";
+        // student.lname = "Hamson";
+        // student.age = 17;
+        // // Try-With-Resources: This construct ensures that the Scanner (or any other
+        // // AutoCloseable resource) is closed automatically at the end of the block,
+        // // regardless of whether an exception is thrown.
+        // try (Scanner scanner = new Scanner(System.in)) {
+        //     // Scanner scanner = new Scanner(System.in);
+        //     System.out.println("Enter Discipline");
+        //     String discipline = scanner.nextLine();
+        //     student.setDiscipline(discipline);
+        //     student.eatTime();
+        //     student.sleepTime();
+        //     student.workType();
+        //     // inner class demonstration
+        //     Student.StudentType studentType = student.new StudentType();
+        //     String stdTyp = studentType.studentType = "Part Time";
+        //     int wrkHrs = studentType.displayWorkHours();
+        //     System.out.println("My name is >>>>>  " + student.fname + student.lname + " and i am " + student.age
+        //             + ", i am studying " + student.getDiscipline() + " " + stdTyp + " I work" + wrkHrs + " per week");
 
-            Worker worker = new Worker();
-            worker.fname = "Donald";
-            worker.lname = "Smith";
-            worker.age = 37;
-            System.out.println("Enter Salary");
-            double salary = scanner.nextDouble();
-            System.out.println(
-                    "My name is >>>>>  " + worker.fname + worker.lname + " and i am " + worker.age + " earning:"
-                            + salary);
-            worker.eatTime();
-            worker.sleepTime();
-            worker.workType();
-        }
+        //     Worker worker = new Worker();
+        //     worker.fname = "Donald";
+        //     worker.lname = "Smith";
+        //     worker.age = 37;
+        //     System.out.println("Enter Salary");
+        //     double salary = scanner.nextDouble();
+        //     System.out.println(
+        //             "My name is >>>>>  " + worker.fname + worker.lname + " and i am " + worker.age + " earning:"
+        //                     + salary);
+        //     worker.eatTime();
+        //     worker.sleepTime();
+        //     worker.workType();
+        // }
 
-        // called directly cos its a static method
-        staticMethod();
+        // // called directly cos its a static method
+        // staticMethod();
 
-        // needs to create the instance of its object before it is called
-        Main main = new Main();
-        main.publicMethod();
+        // // needs to create the instance of its object before it is called
+        // Main main = new Main();
+        // main.publicMethod();
 
-        LocalDate date = LocalDate.now();
-        LocalTime time = LocalTime.now();
-        System.out.println(date + " " + time);
+        // LocalDate date = LocalDate.now();
+        // LocalTime time = LocalTime.now();
+        // System.out.println(date + " " + time);
 
-        LocalDateTime dateTime = LocalDateTime.now();
+        // LocalDateTime dateTime = LocalDateTime.now();
 
-        DateTimeFormatter formatedDateTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String stringifiedFormattedDateTime = dateTime.format(formatedDateTime);
-        System.out.println(dateTime + " <<<<>>>>" + stringifiedFormattedDateTime);
+        // DateTimeFormatter formatedDateTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        // String stringifiedFormattedDateTime = dateTime.format(formatedDateTime);
+        // System.out.println(dateTime + " <<<<>>>>" + stringifiedFormattedDateTime);
+
+        ArrayLearning arrayLearning = new ArrayLearning();
 
     }
 }
